7fb584888c4826ab57462edc9e9686bad5edb05f
전성우 <81775633+zhdkffk8210@users.noreply.github.com>
2026-04-24 16:20:24 +0900

    fix:예외 처리 통합 및 인증 설정 정리

--- src/main/java/com/goorm/shoppingmall/global/error/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.goorm.shoppingmall.global.error;
-
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
